fe2ceb2006fc7765104731e76af19ae9cc4ef766
Paul Campbell <paulgcampbell@gmail.com>
2011-08-31 17:53:48 +0100

    added style sheets

--- Solutions/Lettings.Web.Mvc/Controllers/ViewModels/LoginModel.cs
+++ Solutions/Lettings.Web.Mvc/Controllers/ViewModels/LoginModel.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace Lettings.Web.Mvc.Controllers.ViewModels
 {
     public class LoginModel
     {
+        [DisplayName("Email")]
         [Required(ErrorMessage = "Email required")]
         public string EmailAddress { get; set; }
         
         [Required(ErrorMessage = "Password required")]
+       
         public string Password { get; set; }
 
+         [DisplayName("Remember me on this computer")]
         public bool PersistentLogin { get; set; }
 
     }
