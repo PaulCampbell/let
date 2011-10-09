5c811bbff12281960b7dff07d45b956bdacfbce9
Paul Campbell <paulgcampbell@gmail.com>
2011-10-09 20:16:51 +0100

    account management

---
+++ Solutions/Lettings.Domain/Contracts/Queries/OfficesOfAgentSpecification.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpArch.Domain.Specifications;
+using System.Linq.Expressions;
+
+namespace Lettings.Domain.Contracts.Queries
+{
+    public class OfficesOfAgentSpecification : QuerySpecification<Office>
+    {
+        private readonly int agentId;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProfileByUserNameSpecification"/> class.
+        /// </summary>
+        /// <param name="userName">
+        /// The user name.
+        /// </param>
+        public OfficesOfAgentSpecification(int agentId)
+        {
+            this.agentId = agentId;
+        }
+
+        public int AgentId
+        {
+            get { return this.agentId; }
+        }
+
+        public override Expression<Func<Office, bool>> MatchingCriteria
+        {
+            get { return o => o.Agent.Id == this.agentId ; }
+        }
+    }
+}
